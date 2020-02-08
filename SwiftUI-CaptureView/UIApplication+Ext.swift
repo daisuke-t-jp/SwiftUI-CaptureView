9ed23d8510c7f7d1a87fdbca3485344b85afe78d
Daisuke TONOSAKI <daisuke.t.jp@gmail.com>
2020-02-08 15:42:20 +0900

    Update get keyWindow method

--- SwiftUI-CaptureView/UIApplication+Ext.swift
+++ SwiftUI-CaptureView/UIApplication+Ext.swift
@@ -11,7 +11,16 @@ import UIKit
 extension UIApplication {
     
     static func image() -> UIImage? {
-        guard let rootViewController = UIApplication.shared.windows[0].rootViewController else {
+        guard let keyWindow = UIApplication.shared.connectedScenes
+            .filter({$0.activationState == .foregroundActive})
+            .map({$0 as? UIWindowScene})
+            .compactMap({$0})
+            .first?.windows
+            .filter({$0.isKeyWindow}).first else {
+                return nil
+        }
+        
+        guard let rootViewController = keyWindow.rootViewController else {
             return nil
         }
         
